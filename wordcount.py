bef2842bb633688fc51d6766467ce39c3308cf0f
Rocky Duan <dementrock@gmail.com>
2011-11-25 20:24:58 -0800

    Pythonize the program since I'm boring...

--- wordcount.py
+++ wordcount.py
@@ -1,54 +1,37 @@
 import sys
 
-IGNORE = ['a', 'the', 'are', 'were', 'is', 'am', 'as', 'to', 'than',\
-        'yes', 'no', 'and', 'was', 'that', 'of', 'be', 'will', 'in',\
-        'from', 'with', 'for', 'all', 'by', 'not', 'have', 'must',\
-        'this', 'there', 'it', 'which', 'at', 'an', 'would', 'had',\
-        'about', 'when', 'but', 'how', 'what', 'on', 'or', 'its',\
+IGNORE = ['a', 'the', 'are', 'were', 'is', 'am', 'as', 'to', 'than',
+        'yes', 'no', 'and', 'was', 'that', 'of', 'be', 'will', 'in',
+        'from', 'with', 'for', 'all', 'by', 'not', 'have', 'must',
+        'this', 'there', 'it', 'which', 'at', 'an', 'would', 'had',
+        'about', 'when', 'but', 'how', 'what', 'on', 'or', 'its',
         'into', 'through']
 
+ITEM_PER_LINE = 3
+ITEM_WIDTH = 15
+
 def get_dict(filename):
-    f = open(filename, 'rU')
-    words = f.read().split()
-    dict = {}
-    for word in words:
-        if word.lower() in IGNORE:
-            continue
-        if word.lower() not in dict:
-            dict[word.lower()] = 1
-        else:
-            dict[word.lower()] += 1
-    f.close()
-    return dict
+    from collections import Counter 
+    valid_words = [ word.lower() for word in open(filename, 'rU').read().split() if word.lower() not in IGNORE ]
+    return Counter(valid_words)
 
 def print_top(max_num, filename, outfilename):
     num = int(max_num)
-    dict = get_dict(filename)
-    f = open(outfilename, 'w')
-    cnt = 0
-    tmp = 0
-    for a, b in sorted(dict.items(), key=lambda x: x[1], reverse=True):
-        tmp += 1
-        st = a + ' ' + str(b)
-        if tmp != 3:
-            f.write(st.ljust(15))
-        else:
-            f.write(st + '\n')
-            tmp = 0
-        print a, b
-        cnt += 1
-        if cnt == num: break
-    f.write('\n')
-    f.write('# of most frequent words: ' + max_num + '\n')
-    f.write('Source file: ' + filename + '\n')
-    f.close()
+    freq_list = get_dict(filename).most_common(num)
+    freq_list = zip(freq_list, range(1, len(freq_list) + 1))
+    item_wrapper = lambda x, index: x.ljust(ITEM_WIDTH) if index % ITEM_PER_LINE else x + '\n'
+    output = ''.join([item_wrapper('%s %d' % (item[0], item[1]), index) for item, index in freq_list])
+    if not output.endswith('\n'):
+        output += '\n'
+    output += '# of most frequent words: %s\nSource file: %s\n' % (max_num, filename)
+    open(outfilename, 'w').write(output)
 
 def main():
-    #print 'usage: python wordcount.py NUMBER_OF_MOST_FREQUENT_WORDS INPUT_FILENAME OUTPUT_FILENAME'
-    max_num = sys.argv[1]
-    filename = sys.argv[2]
-    outfilename = sys.argv[3]
-    print_top(max_num, filename, outfilename)
+    if len(sys.argv) == 2 and sys.argv[1] in ['-h', '--help']:
+        print 'usage: python wordcount.py NUMBER_OF_MOST_FREQUENT_WORDS INPUT_FILENAME OUTPUT_FILENAME'
+    else:
+        max_num, filename, outfilename = sys.argv[1:]
+        print_top(max_num, filename, outfilename)
 
 if __name__ == '__main__':
-  main()
+    main()
